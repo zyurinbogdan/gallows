d5e1fbffc2d46bdb15c2a27a2b22cde44ad4689f
Web <web@it.step>
2022-10-18 18:49:38 +0300

    fooAddLetter

---
+++ index.js
@@ -0,0 +1,10 @@
+function addLetter (guess, word, answerArr, remainingLetters, attempts) {
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === guess && answerArr[i] === '_') {
+            answerArr[i] === guess;
+            remainingLetters--;
+        } else {
+            attempts--;
+        }
+    }
+}
