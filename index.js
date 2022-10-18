d23efd42586ef2e4a4fd29625636c7ea800b2ad0
Web <web@it.step>
2022-10-18 21:22:51 +0300

    18.10.2022

--- index.js
+++ index.js
@@ -1,28 +1,60 @@
+const answer = document.getElementsByClassName('answer')[0];
+const buttonOk = document.getElementById('knopkaOk');
+const input = document.getElementById('name');
+
+buttonOk.addEventListener('click', () => {
+    addLetter(input.value)
+})
+
+
+ const words = [
+    'монитор',
+    'кабинет',
+    'арбуз',
+    'ручка',
+    'машина',
+    'морковка',
+    'школа',
+    'телерадиокомпания',
+    'еж',
+];
+
+function getRandomArrayElement (words) {
+    return words[Math.floor(Math.random()*words.length)];
+ }
+
+const word = getRandomArrayElement(words);
+
+const attempts = 3;
 
 
 
-function setupAnswerArr (word) {
-    
-    let answerArr = [];
 
+(function setupAnswerArr (word) {
     for (let i = 0; i < word.length; i++) {
-        answerArr[i] = '_';
+        const newSpan = document.createElement('span');
+        newSpan.classList.add('letter');
+        newSpan.innerText = '_';
+        answer.append(newSpan);
+        console.log(word);
     }
+})(word);
 
-    return answerArr;
+
+
+function checkUnderscore () {
+    const answerCollection = [...document.getElementsByClassName('letter')];
+    answerCollection.every()
 }
 
-function validation (guess) {
-    if( /[а-я]/.test(guess) ) {
-        function addLetter (guess, word, answerArr, remainingLetters, attempts) {
-            for (let i = 0; i < word.length; i++) {
-                if (word[i] === guess && answerArr[i] === '_') {
-                    answerArr[i] === guess;
-                    remainingLetters--;
-                } else {
-                    attempts--;
-                }
-            }
+
+function addLetter (guess, word) {
+    const answerCollection = document.getElementsByClassName('letter');
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === guess) {
+            answerCollection[i].innerText = guess;
         }
     }
 }
+
+
